64a5b868b316519045842c8bb343f2cbeee39fb3
AvelinoC5 <avelino_63@hotmail.com>
2022-08-13 16:54:54 -0500

    Upload ctrl.c

---
+++ ctrl.c
@@ -0,0 +1,12 @@
+#include "shell.h"
+
+/**
+* handle - A function to handle Ctr + C signal.
+* @signals: The signal to handle.
+* Return: Nothing.
+*/
+
+	void handle(int signals){
+	(void)signals;
+	write(STDOUT_FILENO, "\n$ ", 4);
+	}
